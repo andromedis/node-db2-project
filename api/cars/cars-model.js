ff9309600dc4f936006da476766155603907ba25
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-04-13 20:24:30 +0100

    cars model built

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,23 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+    // `getAll` resolves to an array of car records (or an empty array)
+    return db('cars')
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = (id) => {
+    // `getById` resolves to a car record by the given id
+    return db('cars').where({ id }).first()
 }
 
-const create = () => {
-  // DO YOUR MAGIC
+const create = async (car) => {
+    // `create` resolves to the newly created car record
+    const [ id ] = await db('cars').insert(car)
+    return getById(id)
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create
 }
